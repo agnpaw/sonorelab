50387846ac453f2b51b48810c1e90d97e8e73146
Agnieszka Pawelec <agnieszka@iMac-Agnieszka.local>
2026-02-13 16:30:52 +0100

    ready html, css, js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector('.hamburger'); 
+const navList = document.querySelector('.nav-list'); 
+hamburger.addEventListener('click', () => { 
+  navList.classList.toggle('active'); });
